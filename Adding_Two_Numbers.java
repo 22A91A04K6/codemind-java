70a9797567d9916286e9b148564e43bc090d9712
22A91A04K6 <119719418+22A91A04K6@users.noreply.github.com>
2023-05-05 11:23:18 +0530

    import java.util.*; public class Adding {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=a+b;         System.out.println(c);     } }

--- Adding_Two_Numbers.java
+++ Adding_Two_Numbers.java
@@ -1,15 +1,13 @@
 import java.util.*;
 public class Adding
 {
-    
     public static void main(String args[])
     {
-        int a,b;
         Scanner sc=new Scanner(System.in);
+        int a,b,c;
         a=sc.nextInt();
         b=sc.nextInt();
-       System.out.println(a+b);
+        c=a+b;
+        System.out.println(c);
     }
-     
-    
 }
